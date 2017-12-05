d8c3df63b477eaea99131ea62d6b3f80ec39becc
Renne Sampaio <rennesampaio97@gmail.com>
2017-12-05 15:26:52 -0300

    atributo size adicionado e construtor inicial implementado

--- Sources/SGObject.hx
+++ Sources/SGObject.hx
@@ -1,7 +1,15 @@
 package;
 
+using kha.math.FastVector2;
+
 class SGObject {
 	var transform:SGTransform;
+	var size: FastVector2;
+
+	public function new() {
+		transform = new SGTransform();
+	}
+
 	function update():Void {}
 	function render():Void {}
 }
