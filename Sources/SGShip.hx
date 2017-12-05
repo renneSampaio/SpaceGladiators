c038513a0678ab9e27bc2855f413afa415c6e660
Renne Sampaio <rennesampaio97@gmail.com>
2017-12-05 15:27:43 -0300

    Implementação inicial da nave

---
+++ Sources/SGShip.hx
@@ -0,0 +1,19 @@
+package;
+
+using kha.math.FastVector2;
+
+class SGShip extends SGObject {
+	var vel: FastVector2;
+
+	public function new() {
+		super();
+	}
+
+	public override function update() {
+
+	}
+
+	public override function render() {
+		
+	}
+}
