08519a1eba4de2369b76e7a09e482c9559dd1fe1
Renne Sampaio <rennesampaio97@gmail.com>
2017-12-07 19:18:21 -0300

    Adicionado input do player 2

--- Sources/SGShip.hx
+++ Sources/SGShip.hx
@@ -19,7 +19,7 @@ class SGShip extends SGObject {
 
 	public var bullets: Array<SGProjectile>;
 
-	public function new(x: FastFloat, y: FastFloat, speed: FastFloat) {
+	public function new(x: FastFloat, y: FastFloat, speed: FastFloat, playerNumber: Int) {
 		super();
 		transform.SetPosition(new FastVector2(x, y));
 		this.vel = new FastVector2(0, 0);
@@ -28,12 +28,24 @@ class SGShip extends SGObject {
 
 		bullets = new Array<SGProjectile>();
 
-		SGInputManager.Get().AddUpDownObserver(KeyCode.Numpad7, function () { this.right    = true;}, function () { this.right    = false;});
-		SGInputManager.Get().AddUpDownObserver(KeyCode.Numpad9, function () { this.left     = true;}, function () { this.left     = false;});
-		SGInputManager.Get().AddUpDownObserver(KeyCode.Numpad2, function () { this.forward  = true;}, function () { this.forward  = false;});
-		SGInputManager.Get().AddUpDownObserver(KeyCode.Numpad8, function () { this.backward = true;}, function () { this.backward = false;});
+		var playerKeys = {left: KeyCode.F13, right: KeyCode.F13, forward: KeyCode.F13, backward: KeyCode.F13, fire: KeyCode.F13};
 
-		SGInputManager.Get().AddUpObserver(KeyCode.Numpad5, FireBullet);
+		if (playerNumber == 1) {
+			playerKeys.right   = KeyCode.Numpad9; playerKeys.left     = KeyCode.Numpad7;
+			playerKeys.forward = KeyCode.Numpad8; playerKeys.backward = KeyCode.Numpad2;
+			playerKeys.fire    = KeyCode.Numpad5;
+		} else {
+			playerKeys.right   = KeyCode.E; playerKeys.left       = KeyCode.Q;
+			playerKeys.forward = KeyCode.W; playerKeys.backward = KeyCode.X;
+			playerKeys.fire    = KeyCode.S;
+		}
+
+		SGInputManager.Get().AddUpDownObserver(playerKeys.right, function () { this.right    = true;}, function () { this.right    = false;});
+		SGInputManager.Get().AddUpDownObserver(playerKeys.left, function () { this.left     = true;}, function () { this.left     = false;});
+		SGInputManager.Get().AddUpDownObserver(playerKeys.forward, function () { this.forward  = true;}, function () { this.forward  = false;});
+		SGInputManager.Get().AddUpDownObserver(playerKeys.backward, function () { this.backward = true;}, function () { this.backward = false;});
+
+		SGInputManager.Get().AddUpObserver(playerKeys.fire, FireBullet);
 	}
 
 	public function FireBullet() {
